0ac9287e3d2043a8dc04431590596ac81807f5bd
Malek Smida <malek.ssmida@gmail.com>
2023-02-19 17:42:37 +0100

    Refactor: footer

--- sections/index.ts
+++ sections/index.ts
@@ -1,4 +1,3 @@
-export { default as Footer } from './Footer';
 export { default as Intro } from './Intro';
 export { default as About } from './About';
 export { default as Career } from './Career';
