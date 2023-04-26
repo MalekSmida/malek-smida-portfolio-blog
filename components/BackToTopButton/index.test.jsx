009768f9a2628392e366f4990322e6025f67b031
Malek Smida <malek.ssmida@gmail.com>
2023-04-26 14:31:26 +0200

    Test: backtotop button

--- components/BackToTopButton/index.test.jsx
+++ components/BackToTopButton/index.test.jsx
@@ -1,12 +1,27 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 // local files
 import BackToTopButton from './';
 
 describe('Component: BackToTopButton', () => {
-  it('Should render button', async () => {
+  const user = userEvent.setup();
+  beforeEach(() => {
     render(<BackToTopButton />);
+  });
+
+  it('Should render button', async () => {
     const buttonElement = screen.getByRole('button');
     expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('should scroll to the top when clicked', async () => {
+    window.scrollTo = jest.fn(); // mock window.scrollTo
+    const buttonElement = screen.getByRole('button');
+    await user.click(buttonElement);
+    expect(window.scrollTo).toHaveBeenCalledWith({
+      top: 0,
+      behavior: 'smooth',
+    });
   });
 });
