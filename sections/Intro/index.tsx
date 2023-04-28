d831921d28e7b010c7f9674aca734461e7212944
Malek Smida <malek.ssmida@gmail.com>
2023-04-28 14:23:41 +0200

    Refactor: intro mobile style

--- sections/Intro/index.tsx
+++ sections/Intro/index.tsx
@@ -10,15 +10,16 @@ import hyperlinks from '../../utils/hyperlinks';
 function Intro() {
   return (
     <section>
-      <div className="px-4 py-16 mx-auto sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 gap-8 lg:gap-16 lg:grid-cols-2">
-          <div className="group relative hidden h-full order-last lg:inline-block">
+      <div className="mx-auto px-4 py-16 sm:px-6 lg:px-8">
+        <div className="flex flex-col justify-center lg:flex-row">
+          <div className="group relative order-last h-full max-w-lg p-4 lg:max-w-none lg:flex-1 lg:p-0">
             <svg
               className="absolute bottom-6 right-0 transition-transform duration-1000 ease-in-out group-hover:rotate-12"
               height="100%"
               viewBox="0 0 1010 1005"
               fill="none"
-              xmlns="http://www.w3.org/2000/svg">
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <rect
                 x="-74"
                 y="641.438"
@@ -32,7 +33,7 @@ function Intro() {
 
             <Image src={MalekJammel} alt="Malek Smida jebba" priority />
 
-            <div className="absolute bottom-32 right-80 z-10 w-3/4 border border-black transition-transform duration-300 ease-in-out group-hover:scale-110">
+            <div className="absolute bottom-20 right-0 z-10 w-3/4 border border-black transition-transform duration-300 ease-in-out group-hover:scale-110 lg:bottom-32 lg:right-80">
               <Image
                 src={SirineReview}
                 alt="Sirine ben Hamida linkedin's recommendation"
@@ -40,23 +41,29 @@ function Intro() {
               />
             </div>
           </div>
-          <div className="lg:py-24">
+          <div className="pb-20 text-center lg:flex-1 lg:py-24 lg:text-start">
             <h1 className="text-3xl font-bold sm:text-4xl">Malek Smida</h1>
-            <h2 className="mt-4 text-xl sm:text-2xl text-gray-500">Software Engineer</h2>
-            <h2 className="mt-2 text-xl sm:text-2xl text-gray-500">Full stack Web/Mobile</h2>
+            <h2 className="mt-4 text-xl text-gray-500 sm:text-2xl">
+              Software Engineer
+            </h2>
+            <h2 className="mt-2 text-xl text-gray-500 sm:text-2xl">
+              Full stack Web/Mobile
+            </h2>
             <Link href={`/experience/coming-soon`}>
               <a
-                className="mt-4 sm:mt-8 relative inline-flex items-center px-8 py-3 overflow-hidden text-white bg-indigo-600 rounded group border border-indigo-600 hover:bg-transparent hover:text-indigo-600 active:text-indigo-500 focus:outline-none focus:ring cursor-pointer"
+                className="group relative mt-4 inline-flex cursor-pointer items-center overflow-hidden rounded border border-indigo-600 bg-indigo-600 px-8 py-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500 sm:mt-8"
                 href={hyperlinks.CVPDF}
                 rel="noopener noreferrer"
-                target="_blank">
-                <span className="absolute right-0 transition-transform translate-x-full group-hover:-translate-x-4">
+                target="_blank"
+              >
+                <span className="absolute right-0 translate-x-full transition-transform group-hover:-translate-x-4">
                   <svg
-                    className="w-5 h-5"
+                    className="h-5 w-5"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke="currentColor">
+                    stroke="currentColor"
+                  >
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
